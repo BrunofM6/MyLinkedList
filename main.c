a306a5437ebbef95debb3573dab02c3ba84ebfc5
BrunoM6 <bfmbruno2003@gmail.com>
2024-03-12 15:42:31 +0000

    Merged Branches.

--- main.c
+++ main.c
@@ -9,7 +9,21 @@ typedef struct {
   Node *current;
 } LinkedList;
 
+void printMenu(){
+    printf("Select what you want to do:\n");
+    printf("1 - Add node to the beginning.\n");
+    printf("2 - Add node to the end.\n");
+    printf("3 - Remove head.");
+    printf("4 - Remove tail.");
+    printf("5 - Remove tail.");
+    printf("6 - Add element at n position.");
+    printf("7 - Remove element at n position.");
+    printf("8 - Quit.")
+}
 int main(){
-    
+    LinkedList mainList;
+    while(true){
+        
+    }
     return 0;
 }
